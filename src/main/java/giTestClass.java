d20ae850ae7cc17e68f6ba74d9c7154647b6b894
cyc <slkacyc@gmail.com>
2024-05-09 12:47:07 +0800

    第三次提交测试

--- src/main/java/giTestClass.java
+++ src/main/java/giTestClass.java
@@ -2,4 +2,12 @@ public class giTestClass {
     public static void main(String[] args) {
         System.out.println("修改~！");
     }
+
+    public void aa(){
+        int a;
+    }
+    public void bb(){
+        int b;
+    }
+
 }
--- src/main/java/test2.java
+++ src/main/java/test2.java
@@ -2,4 +2,10 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("测试文件2");
     }
+
+    public void hh(){
+        int h;
+    }
+
+
 }
